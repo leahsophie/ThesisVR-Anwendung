ecf0441f85363a31335a8ff35db382d9248caaa1
DESKTOP-E8OEJA7\schnu <markus-ws-weiss@outlook.de>
2019-06-04 23:30:28 +0200

    Add Camera movement

---
+++ UnityProject/Assets/CameraMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float minimumVert = -45.0f;
+    public float maximumVert = 45.0f;
+
+    public float sensHorizontal = 10.0f;
+    public float sensVertical = 10.0f;
+
+    public float _rotationX = 0;
+
+    private CharacterController _charCont;
+    public float gravity = -9.81f;
+
+    private void Start()
+    {
+        _charCont = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Rotate(0, Input.GetAxis("Mouse X") * sensHorizontal, 0);
+
+        _rotationX -= Input.GetAxis("Mouse Y") * sensVertical;
+        _rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert);
+
+        float rotationY = transform.localEulerAngles.y;
+
+        transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+
+
+    }
+
+}
